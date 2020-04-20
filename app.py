a740af30800866ba0ca682e1ced2afbb753dffab
SADAT <sadat.himel@gmail.com>
2020-04-21 00:11:04 +0600

    add some string

--- app.py
+++ app.py
@@ -1,11 +1,52 @@
-birth_year = input('Birth year: ')
-age = 2020 - int(birth_year)
-print(age)
+# birth_year = input('Birth year: ')
+# print(type(birth_year))
+# age = 2020 - int(birth_year)
+# print(type(age))
+# print(age)
 
-name = "himel"
-print(name)
+# weight_lbs = input('weight(lbs): ')
+# weight_kg = int(weight_lbs)* 0.45
+# print(weight_kg)
 
+# string
 
+# course = "Python's Course for Beginners"
+# course = '''
+# Hi jhon
+# this is a
+# beginners
+# course
+# '''
+
+# course = 'Python for Beginners'
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:])
+# another = course[:]
+# print(another)
+
+# name = 'Jennifer'
+# print(name[1:-1])
+
+# Pormative string
+# first = 'John'
+# last = "Smit"
+# message = first + ' [' + last + '] is a coder'
+# msg = f'{first} [{last}] is a coder'
+# print(message)
+
+course = 'Python for Beginners'
+
+# print(course.upper())
+# print(course.lower())
+# print(len(course))
+# print(course)
+
+# print(course.replace('P', 'h'))
+
+print('python' in course)
+time = 48.39
+print(time)
 
 
 
